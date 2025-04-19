757d45a3522249ad696f457e5d85d1fd3f62ff2e
agliucci <aliucci@umass.edu>
2025-04-19 17:55:40 -0400

    Initialize Express server with CORS and JSON middleware

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,18 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+
 
 const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send("API is up an running");
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
 
