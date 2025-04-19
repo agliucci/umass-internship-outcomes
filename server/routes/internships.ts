7e8f6841de91ae061ce5ebb33cea85ab72e74bd2
agliucci <aliucci@umass.edu>
2025-04-19 19:19:58 -0400

    Added validation

--- server/routes/internships.ts
+++ server/routes/internships.ts
@@ -4,14 +4,44 @@ import { internships, Internship } from "../internship";
 // Create a new router instance to define and handle routes specific to internships, allowing for modular route management
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', (req: express.Request, res: express.Response): void => {
     const newInternship: Internship = req.body;
+
+    const { company, position, pay, major, gpa, experience } = newInternship;
+
+    if (
+        !company ||
+        !position ||
+        typeof pay !== 'number' ||
+        !major ||
+        !gpa ||
+        !experience
+    ) {
+        res.status(400).json({error: "All fields must be valid."});
+    }
+
     internships.push(newInternship);
     res.status(201).json({ message: "Internship submitted!" });
 });
 
 router.get('/', (req, res) => {
-    res.json(internships);
+    const { major, company, gpa } = req.query;
+
+    let filtered = internships;
+
+    if (major) {
+        filtered = filtered.filter(internship => internship.major.toLowerCase() === (major as string).toLowerCase());
+    }
+
+    if (company) {
+        filtered = filtered.filter(internship => internship.company.toLowerCase() === (company as string).toLowerCase());
+    }
+
+    if (gpa) {
+        filtered = filtered.filter(internship => internship.gpa === Number(gpa));
+    }
+
+    res.json(filtered);
 });
 
 export default router;
